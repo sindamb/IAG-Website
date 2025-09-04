9fea9178be8450637225c4da1cbd4a35497fcbcc
sindambiw <sindambiwesylvere2018@gmail.com>
2025-09-05 00:00:56 +0300

    updates

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(Server is running on http://0.0.0.0:${PORT});
+    console.log("Server is running on http://0.0.0.0:${PORT}");
 });
